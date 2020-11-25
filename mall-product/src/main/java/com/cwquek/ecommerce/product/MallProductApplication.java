a418996bae24f4ccf37b662d2183e3bc08b68206
quek0504 <quek0504@gmail.com>
2020-11-25 14:19:41 +0800

    simple CRUD test

--- mall-product/src/main/java/com/cwquek/ecommerce/product/MallProductApplication.java
+++ mall-product/src/main/java/com/cwquek/ecommerce/product/MallProductApplication.java
@@ -1,8 +1,10 @@
 package com.cwquek.ecommerce.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.cwquek.ecommerce.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
--- mall-product/src/test/java/com/cwquek/ecommerce/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/cwquek/ecommerce/product/MallProductApplicationTests.java
@@ -1,13 +1,41 @@
 package com.cwquek.ecommerce.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.cwquek.ecommerce.product.entity.BrandEntity;
+import com.cwquek.ecommerce.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+
+//		BrandEntity brandEntity = new BrandEntity();
+//		brandEntity.setBrandId(1L);
+//		brandEntity.setDescript("Huawei");
+
+//		brandEntity.setName("Huawei");
+//		brandService.save(brandEntity);
+//		System.out.println("Saving completed...");
+
+//		brandService.updateById(brandEntity);
+
+
+//		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+//		list.forEach((item) -> {
+//			System.out.println(item);
+//		});
+//
+//		brandService.removeById(1L);
+
 	}
 
 }
